716d240a2d13b7e38faf4ca1eb54e94f87f49752
wigens7 <151595269+wigens7@users.noreply.github.com>
2026-09-05 22:41:29 +0200

    Complete web FCM notification handling

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { initializeFirebase } from "./lib/firebase";
 import "./index.css";
+
+initializeFirebase();
 
 createRoot(document.getElementById("root")!).render(<App />);
